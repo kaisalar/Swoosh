540b8cb3e76c5011cdbf8e95ad726446cf3149b9
kaisalar <kais.alar@outlook.com>
2019-02-11 12:20:58 -0800

    Welcome Layout added

--- app/src/main/java/com/ksmartsolutions/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/ksmartsolutions/swoosh/WelcomeActivity.kt
@@ -1,12 +1,22 @@
 package com.ksmartsolutions.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
+
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
+
+
     }
 }
