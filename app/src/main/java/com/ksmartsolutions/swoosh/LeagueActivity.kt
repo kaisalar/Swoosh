1536732dc551909286498c0fd7b7d87441bbb20e
kaisalar <kais.alar@outlook.com>
2019-02-12 02:02:17 -0800

    Desired League Layout added

--- app/src/main/java/com/ksmartsolutions/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/ksmartsolutions/swoosh/LeagueActivity.kt
@@ -4,23 +4,15 @@ import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
-import kotlinx.android.synthetic.main.activity_welcome.*
 
-
-class WelcomeActivity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_league)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         }
-
-        getStartedButton.setOnClickListener {
-
-        }
-
-
     }
 }
