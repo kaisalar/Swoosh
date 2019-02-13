2a6c73eff40b2b876d8190d9cfc23599cae7c89d
kaisalar <kais.alar@outlook.com>
2019-02-13 05:30:09 -0800

    Passing Parcelable Data 2

--- app/src/main/java/com/ksmartsolutions/swoosh/Controllers/LeagueActivity.kt
+++ app/src/main/java/com/ksmartsolutions/swoosh/Controllers/LeagueActivity.kt
@@ -12,6 +12,16 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        player = savedInstanceState?.getParcelable(EXTRA_PLAYER) as Player
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
