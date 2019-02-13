33e88b8efa81d4c8c2e88ff28318501e9ddacdbe
kaisalar <kais.alar@outlook.com>
2019-02-13 02:18:43 -0800

    make th finish activity and pass data to it2

--- app/src/main/java/com/ksmartsolutions/swoosh/Controllers/SkillsActivity.kt
+++ app/src/main/java/com/ksmartsolutions/swoosh/Controllers/SkillsActivity.kt
@@ -42,5 +42,7 @@ class SkillsActivity : BaseActivity() {
                 startActivity(finishIntent)
             }
         }
+
+        backButton.setOnClickListener { onBackPressed() }
     }
 }
